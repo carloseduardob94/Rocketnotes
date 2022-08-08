976b5b1e0fd0e218d72d37a5697a84247ebee492
Eduardo <educonts08@gmail.com>
2022-08-08 15:48:30 -0300

    ButtonText component created

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,19 @@
 import { Container, Links} from './styles';
 
-import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
-import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
 
   return(
     <Container>
       <Header/>
+
+      <ButtonText title="Excluir Nota"/>
+
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://rocketseat.com.br</a></li>
