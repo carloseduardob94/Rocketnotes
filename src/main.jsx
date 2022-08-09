a46f5ec1924e1b9c062cec8cded7b48c130e144c
Eduardo <educonts08@gmail.com>
2022-08-09 14:51:42 -0300

    Creating Home Page and Input component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details'
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
+import theme from './styles/theme'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = { theme }>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
