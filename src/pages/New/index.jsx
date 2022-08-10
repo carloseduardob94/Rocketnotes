67fcd6bbaf3fd93679721229bd578db45ebe22cc
Eduardo <educonts08@gmail.com>
2022-08-10 15:18:58 -0300

    created New page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -4,6 +4,7 @@ import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 export function New(){
   return(
@@ -24,6 +25,14 @@ export function New(){
             <NoteItem value="https://rocketseat.com.br"/>
             <NoteItem isNew placeholder="Novo link"/>
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+            <NoteItem value="react"/>
+            <NoteItem isNew placeholder="Novo tag"/>
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
