f0a33c4aabc4d13f4c3b08d7214195f2a7ae9aeb
Eduardo <educonts08@gmail.com>
2022-08-10 15:04:13 -0300

    created NoteItem component

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Form } from './styles';
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
 
 export function New(){
   return(
@@ -15,6 +16,7 @@ export function New(){
           </header>
 
           <Input placeholder="Título" />
+          <Textarea placeholder="Observações"/>
         </Form>
       </main>
     </Container>
