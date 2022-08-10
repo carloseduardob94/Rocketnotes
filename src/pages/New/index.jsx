d6c657ec3aa71bf51b5c64c7a0d7337b70d4400c
Eduardo <educonts08@gmail.com>
2022-08-10 14:47:02 -0300

    created Textarea component

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,22 @@
+import { Container, Form } from './styles';
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+
+export function New(){
+  return(
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="#">Voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
