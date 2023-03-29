10463dc15f90811bafc20c7a88615688687c3379
Eduardo <educonts08@gmail.com>
2023-03-29 14:46:20 -0300

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,19 @@ import { Container, Profile, Logout } from './styles';
 
 import { api } from '../../services/api'
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+import { useNavigate } from 'react-router-dom';
 
 export function Header(){
   const { signOut, user } = useAuth()
 
+  const navigate = useNavigate()
+
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  function handleSignOut(){
+    navigate('/')
+    signOut()
+  }
 
 
   return(
@@ -22,7 +30,7 @@ export function Header(){
         </div>
       </Profile>
 
-      <Logout onClick={signOut} >
+      <Logout onClick={handleSignOut} >
         <RiShutDownLine />
       </Logout>
     </Container>
